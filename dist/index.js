942b24441a6b1005e4fedd1536a9b667e16088dc
Elsabe Ros <ElsabeR@discovery.co.za>
2021-08-31 09:35:57 +0200

    Added a header to the comment.

--- dist/index.js
+++ dist/index.js
@@ -12407,7 +12407,7 @@ async function action() {
         if (debugMode) core.info(`changedFiles: ${debug(changedFiles)}`);
 
         const xmlList = await reportJsonAsync
-        let comment = ""
+        let comment = "## Code coverage" + `\n\n`
         for (let value of xmlList) {
             if (debugMode) core.info(`report value: ${debug(value)}`);
             const report = value["report"];
